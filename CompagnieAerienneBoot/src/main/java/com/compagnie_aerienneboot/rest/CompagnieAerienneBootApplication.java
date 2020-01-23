9352d3834ae21b1ead227bf1275d353238fac77c
RomaricKanyamibwa <romaric.kanyamibwa@gmail.com>
2020-01-23 14:59:06 +0100

    Projet Spring Boot

---
+++ CompagnieAerienneBoot/src/main/java/com/compagnie_aerienneboot/rest/CompagnieAerienneBootApplication.java
@@ -0,0 +1,13 @@
+package com.compagnie_aerienneboot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompagnieAerienneBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompagnieAerienneBootApplication.class, args);
+	}
+
+}
---
+++ CompagnieAerienneBoot/src/test/java/com/compagnie_aerienneboot/rest/CompagnieAerienneBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compagnie_aerienneboot.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompagnieAerienneBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
